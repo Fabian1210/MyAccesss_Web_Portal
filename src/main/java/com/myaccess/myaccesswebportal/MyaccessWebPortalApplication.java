85c4e13e8894f81cf0f08d37999d1cf245f6579c
Fabian Tovar <fabian@Fabians-MacBook-Pro.local>
2025-11-19 21:47:04 -0800

    initial commit

---
+++ src/main/java/com/myaccess/myaccesswebportal/MyaccessWebPortalApplication.java
@@ -0,0 +1,13 @@
+package com.myaccess.myaccesswebportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyaccessWebPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyaccessWebPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myaccess/myaccesswebportal/MyaccessWebPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myaccess.myaccesswebportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyaccessWebPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
